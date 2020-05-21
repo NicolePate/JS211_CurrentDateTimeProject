dc31b5fe896298ee15a32a05e6002b9cb9439af2
NicolePate <nicole.pate512@gmail.com>
2020-05-21 14:26:37 -0500

    commit 1

--- main.js
+++ main.js
@@ -16,35 +16,101 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
+const numToString = () => {
+  let num = document.getElementById("numberString").value
+  
+  numberToString = num.toString();
+
+  document.getElementById("stringShow").innerHTML = `" ${numberToString} " `;
+
+console.log(typeof(numberToString));
+}
 
 
 // Write a JavaScript program to convert a string to the number.
 
+const stringToNum = () => {
+  let str = document.getElementById("stringNum").value
+  stringToNumber = parseInt(str);
+  document.getElementById("numberShow").innerHTML = stringToNumber;
+  console.log(typeof(stringToNumber));
+}
+
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
+  
+  // Boolean
   // * Null
   // * Undefined
   // * Number
   // * NaN
   // * String
+
+  const dataData = () => {
+    let data = document.getElementById("dataType").value;
+    if (data === "null")
+    {
+      document.getElementById("dataShow").innerHTML = "null";
+    }
+    else if (data === "true") 
+    {
+      document.getElementById("dataShow").innerHTML = "boolean";
+    }
+    else if (data == "false") 
+    {
+      document.getElementById("dataShow").innerHTML = "boolean";
+    }
+    else if (data <= Number.MAX_VALUE) 
+    {
+      document.getElementById("dataShow").innerHTML = "number";
+    }
+    else {
+      document.getElementById("dataShow").innerHTML = typeof data};
+      console.log(typeof(data))
+  }
   
 
   
 // Write a JavaScript program that adds 2 numbers together.
 
 
+const addTwoNumbers = () => {
+  let a = parseInt(document.getElementById("a").value);
+  let b = parseInt(document.getElementById("b").value);
+  sum = a + b;
+  document.getElementById("sumShow").innerHTML = sum;
+  console.log(sum);
+}
 
 // Write a JavaScript program that runs only when 2 things are true.
-
-
-
 // Write a JavaScript program that runs when 1 of 2 things are true.
+// Write a JavaScript program that runs when both things are not true. 
 
-
-
-// Write a JavaScript program that runs when both things are not true.  
+const bothTrue = () =>{
+  let firstBool = document.getElementById("firstBool").value;
+  let secondBool = document.getElementById("secondBool").value;
+  firstBool = firstBool.toLowerCase();
+  secondBool = secondBool.toLowerCase();
+  firstBool = firstBool.trim();
+  secondBool = secondBool.trim();
+  if(firstBool== "true" && secondBool == "true"){
+    document.getElementById("bothShow").innerHTML = "Both are true";
+    console.log("Both are true");
+  }
+  else if(firstBool == "false" && secondBool == "true" || firstBool == "true" && secondBool == "false"){
+    document.getElementById("bothShow").innerHTML = "Only one is true"
+    console.log("Only one is true");
+  }
+  else if(firstBool == "false" && secondBool == "false"){
+    document.getElementById("bothShow").innerHTML = "Both are false";
+    console.log("Both are false");
+  }
+  else{
+    document.getElementById("bothShow").innerHTML = "You did not put a boolean in the box! Please only put in true or false"
+    console.log("You did not put a boolean in the box! Please only put in true or false");
+  }
+}
 
 // ***************************
 //         PART TWO
